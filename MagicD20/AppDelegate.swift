046d3d3eef813554968bc8b1bf5ceed5f313ca52
generiglesias <giglesia@sfu.ca>
2018-02-08 23:02:11 -0800

    Added launch screen with basic image

--- MagicD20/AppDelegate.swift
+++ MagicD20/AppDelegate.swift
@@ -16,6 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // Ensures that launch/splash screen lasts for 1.5 seconds
+        sleep(UInt32(1.75))
         return true
     }
 
